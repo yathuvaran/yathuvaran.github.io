1d072adc3c0034a580505b2249f52d7271044353
yathuvaran <49103000+yathuvaran@users.noreply.github.com>
2020-03-25 22:19:28 -0400

    added boostrap support

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
-import App from './App';
+import Home from './containers/Home';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Home />
   </React.StrictMode>,
   document.getElementById('root')
 );
