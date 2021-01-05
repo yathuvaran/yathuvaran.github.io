4a544d8e85b79791008ec4bdf66c97044affa1c2
yathuvaran <49103000+yathuvaran@users.noreply.github.com>
2021-01-05 00:04:25 -0500

    fix homescreen for larger displays

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, Button} from 'react-bootstrap';
+import { Container, Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import Typewriter from 'typewriter-effect';
 import Scramble from 'react-scramble';
@@ -7,46 +7,52 @@ import Scramble from 'react-scramble';
 function HomePage() {
 
     return (
-        <div className='transparent_home'>
-            <Container className='typewriter_cont'>
-                <div className='typewriter'>
-                    <Typewriter
-                        options={{
-                            autoStart: true,
-                            loop: true,
-                        }}
-                        onInit={(typewriter) => {
-                            typewriter.typeString('Hello World!')
-                                .pauseFor(250)
-                                .deleteAll()
-                            typewriter.typeString("I'm Yathu,")
-                                .pauseFor(250)
-                                .deleteAll()
-                                .start();
-                        }}>
-                    </Typewriter>
+        <div class="outer">
+            <div class="middle">
+                <div class="inner">
+                    <div className='transparent_home'>
+                        <Container className='typewriter_cont'>
+                            <div className='typewriter'>
+                                <Typewriter
+                                    options={{
+                                        autoStart: true,
+                                        loop: true,
+                                    }}
+                                    onInit={(typewriter) => {
+                                        typewriter.typeString('Hello World!')
+                                            .pauseFor(250)
+                                            .deleteAll()
+                                        typewriter.typeString("I'm Yathu,")
+                                            .pauseFor(250)
+                                            .deleteAll()
+                                            .start();
+                                    }}>
+                                </Typewriter>
+                            </div>
+                        </Container>
+                        <Container className='scrambled_container'>
+                            <div className='scrambled'>
+                                <Scramble
+                                    autoStart={true}
+                                    preScramble={true}
+                                    speed={'medium'}
+                                    noBreakSpace={true}
+                                    text="I'm a third year Software Engineering student at Carleton University"
+                                    steps={[
+                                        {
+                                            action: '-',
+                                            type: 'forward',
+                                        },
+                                    ]}
+                                />
+                            </div>
+                        </Container>
+                        <Container className='btn_cont'>
+                            <Button as={Link} to='/about' className='flow_btn' variant="outline-light">About Me</Button>
+                        </Container>
+                    </div>
                 </div>
-            </Container>
-            <Container className='scrambled_container'>
-                <div className='scrambled'>
-                    <Scramble
-                        autoStart={true}
-                        preScramble={true}
-                        speed={'medium'}
-                        noBreakSpace={true}
-                        text="I'm a third year Software Engineering student at Carleton University"
-                        steps={[
-                            {
-                                action: '-',
-                                type: 'forward',
-                            },
-                        ]}
-                    />
-                </div>
-            </Container>
-            <Container className='btn_cont'>
-                <Button as={Link} to='/about' className='flow_btn' variant="outline-light">About Me</Button>
-            </Container>
+            </div>
         </div>
     );
 }
